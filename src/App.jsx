727c5bd0050c982462c458556a78e1df79fda0d4
ibrahimnivan <ibrahimnivan@gmail>
2023-10-06 17:54:43 +0700

    menambah router

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import AddProduct from './components/AddProduct';
 import ShowProduct from './components/ShowProduct';
 
 function App() {
   return (
-    <div className="container">
-      <div className="columns">
-        <div className="column">
-         <AddProduct />
-        </div>
-        <div className="column">
-         <ShowProduct />
-        </div>
+    <BrowserRouter>
+      <div className="container">
+        <Routes>
+          <Route path='/' element={<ShowProduct />} />
+          <Route path='add' element={<AddProduct />} />
+        </Routes>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
